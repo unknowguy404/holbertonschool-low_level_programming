d823d29db70d3b12b75688cf12ddae9e1a96107f
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-02-19 19:40:44 -0400

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main- Entry point
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
- * Description: A program that prints to standard error
- *
- * Return: Always 1 (success)
+ * Return: Always 1.
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
 	return (1);
 }
