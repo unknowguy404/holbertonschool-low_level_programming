e2f217d75b1940b8e6ab6836b5b9ce8b9478d1f0
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-19 15:02:42 -0400

    Size is not grandeur, and territory does not make a nation

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,16 +1,5 @@
 #include <stdio.h>
-/**
-* main - Entry point
-* 
-*return: Always 0 (Success)
-*/ 
 int main(void)
 {
-int 1;
-char 4;
-long int 4;
-long long int 8;
-float 4;
-return (0);
-}
 
+}
