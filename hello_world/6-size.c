558d87759ee271869a81b4b7ec1f834f369c9da8
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-19 15:55:23 -0400

    Size is not grandeur, and territory does not make a nation

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Prints the size of various types on the computer it is compiled and run on..
- * Return: Always 0 (Success)
+* main - Prints the size of various types on the computer it is compiled and run on..
+* Return: Always 0 (Success)
 */
 
 int main(void)
 {
+
 int i;
 char c;
 long int li;
