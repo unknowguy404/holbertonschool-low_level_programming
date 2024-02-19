f5dadf429f71f826dfeaacbad7c4c75b44ec2f68
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-19 13:40:01 -0400

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
+/**
+*main - Entro point
+*
+*Return: always
+*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
