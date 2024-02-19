53f92bdbccca046aa5578bd3f6f2904a5ed64dd8
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-19 13:18:32 -0400

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,10 +2,6 @@
 
 int main(void)
 {
-    char str[100];
-    printf("Enter a value: ");
-    gets(str);
-    printf("\nYou entered: ");
     puts("Programming is like building a multilingual puzzle");
     return 0;
 }
