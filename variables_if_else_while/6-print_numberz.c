d3d4f909307710978ccaf3239a9d02add8c3ae53
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-20 13:40:15 -0400

    numbers1to9

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-    int n;
+int n;
 
-    for (n = 0; n <= 9; n++)
-    putchar(n + '0');
-    putchar('\n');
-    return (0);
+for (n = 0; n <= 9; n++)
+putchar(n + '0');
+putchar('\n');
+return (0);
 }
