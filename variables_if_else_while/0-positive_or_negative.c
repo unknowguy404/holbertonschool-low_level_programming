7b1e54ac9937618c76f382404d797393ca24a279
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-20 09:06:21 -0400

    Positive anything is better than negative nothing

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,7 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/*
-*Main - Entry point
-*Description: print if a number is positive, negative or zero
+/**
+*main - print if a number is positive, negative or zero
+*
 *Return: Always 0 (Success)
 */
 int main(void)
