10b51c7301299cbbfce530614266950d484076a6
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-20 10:28:23 -0400

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet in uppercase
+ * Description: print the alphabet in uppercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+
+for (c = 'A'; c <= 'Z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
