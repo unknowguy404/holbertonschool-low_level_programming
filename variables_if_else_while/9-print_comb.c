6e2d2c7beddc4add50fa6979af38ca3a4efe5d01
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-20 14:34:24 -0400

    comb

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-    int n;
+int n;
 
-    for (n = 48; n < 58; n++)
-    {
-        putchar(n);
-        if (n != 57)
-        {
-            putchar(44);
-            putchar(32);
-        }
-    }
-    putchar(10);
-    return (0);
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+if (n != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+putchar(10);
+return (0);
 }
