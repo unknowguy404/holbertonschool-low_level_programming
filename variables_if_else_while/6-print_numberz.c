1cfb95cf7c964680be44b3144552277fcc17e3ad
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-20 13:39:32 -0400

    numbers1to9

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 0 to 10
+ * Description: print numbers from 0 to 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int n;
+
+    for (n = 0; n <= 9; n++)
+    putchar(n + '0');
+    putchar('\n');
+    return (0);
+}
