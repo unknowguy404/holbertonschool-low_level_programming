b92df2c34e3838e455192ae902cd3cf49a5883f5
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-20 13:18:51 -0400

    numbers1to10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 0 to 10
+ * Description: print numbers from 0 to 10
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+
+for (n = 0; n <= 10; n++)
+printf("%d", n);
+return (0);
+}
