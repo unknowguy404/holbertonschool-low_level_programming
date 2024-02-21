db2ade5aa1a4d2db1e975025cc6f5743228caa78
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-21 13:24:46 -0400

    24hours

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * Return: void
+ */
+void jack_bauer(void)
+{
+int h1, h2, m1, m2;
+for (h1 = 0; h1 <= 2; h1++)
+{
+for (h2 = 0; h2 < 10; h2++)
+{
+if (h1 == 2 && h2 == 4)
+break;
+for (m1 = 0; m1 < 6; m1++)
+{
+for (m2 = 0; m2 < 10; m2++)
+{
+_putchar(h1 + '0');
+_putchar(h2 + '0');
+_putchar(':');
+_putchar(m1 + '0');
+_putchar(m2 + '0');
+_putchar('\n');
+}
+}
+}
+}
+}
