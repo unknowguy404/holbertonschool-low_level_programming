816d0525dbd7ae025f8ebbd5fe1089ebd0e6fd75
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-21 13:39:30 -0400

    timetable

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+    int i, j, result;
+
+    for (i = 0; i < 10; i++)
+    {
+        _putchar('0');
+        _putchar(',');
+        _putchar(' ');
+        for (j = 1; j < 10; j++)
+        {
+            result = i * j;
+            if (result < 10)
+            {
+                _putchar(' ');
+                _putchar(' ');
+                _putchar(result + '0');
+            }
+            else
+            {
+                _putchar(' ');
+                _putchar((result / 10) + '0');
+                _putchar((result % 10) + '0');
+            }
+            if (j < 9)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+    }
+}
