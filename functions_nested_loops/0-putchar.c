add0ece7ff6d498d3d4daca7cdcf78a03866c057
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-21 10:54:17 -0400

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
-#include "main.h"
+#include <stdio.h>
 /**
-* main - Entry point
-*
-* Description: prints _putchar using putchar prototype *
-* Return: Always 0 (Success)
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * Description: This program prints _putchar, followed by a new line
 */
 int main(void)
 {
-	char str[] = "_putchar";
-	int ch;
-
-	for (ch = 0; ch < 8; ++ch)
-		_putchar(str[ch]);
-	_putchar('\n');
-
-	return (0);
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r'); 
+putchar('\n');
+return (0);
 }
