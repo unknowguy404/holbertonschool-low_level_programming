cdc3b275eba124cd269aa4a3c904b958c1e344d0
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-02-21 18:09:17 -0400

    print_to_98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,25 +1,35 @@
 #include "main.h"
+
 /**
  * print_to_98 - prints all natural numbers from n to 98
  * @n: starting number
+ *
  * Return: void
  */
 void print_to_98(int n)
 {
-int i;
-if (n <= 98)
+if (n < 98)
 {
-for (i = n; i < 98; i++)
+for (n = n; n < 98; n++)
 {
-printf("%d, ", i);
+_putchar(n / 10 + '0');
+_putchar(n % 10 + '0');
+_putchar(',');
+_putchar(' ');
 }
 }
 else
 {
-for (i = n; i > 98; i--)
+for (n = n; n > 98; n--)
 {
-printf("%d, ", i);
+_putchar(n / 10 + '0');
+_putchar(n % 10 + '0');
+_putchar(',');
+_putchar(' ');
 }
 }
-printf("98\n");
+_putchar('9');
+_putchar('8');
+_putchar('\n');
 }
+     
