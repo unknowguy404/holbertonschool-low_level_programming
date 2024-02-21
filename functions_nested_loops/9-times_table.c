92b1c446f4b607355a6ab12036dd96972488fe6a
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-21 13:40:31 -0400

    timetable

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,34 +7,34 @@
  */
 void times_table(void)
 {
-    int i, j, result;
+int i, j, result;
 
-    for (i = 0; i < 10; i++)
-    {
-        _putchar('0');
-        _putchar(',');
-        _putchar(' ');
-        for (j = 1; j < 10; j++)
-        {
-            result = i * j;
-            if (result < 10)
-            {
-                _putchar(' ');
-                _putchar(' ');
-                _putchar(result + '0');
-            }
-            else
-            {
-                _putchar(' ');
-                _putchar((result / 10) + '0');
-                _putchar((result % 10) + '0');
-            }
-            if (j < 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+for (i = 0; i < 10; i++)
+{
+ _putchar('0');
+_putchar(',');
+_putchar(' ');
+for (j = 1; j < 10; j++)
+{
+result = i * j;
+if (result < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(result + '0');
+}
+else
+{
+_putchar(' ');
+_putchar((result / 10) + '0');
+_putchar((result % 10) + '0');
+}
+if (j < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
 }
