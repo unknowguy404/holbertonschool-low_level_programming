044d2f75899cf64848b0a45c7bc9e9842467d64b
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-21 11:31:07 -0400

    0-putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
+
 /**
- * main - Entry point
+ * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
-*/
+ */
 int main(void)
 {
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('\n');
-return (0);
-}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
+	return (0);
+}
