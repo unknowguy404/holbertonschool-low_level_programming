e6ea4a50d4b1a5873a65544ccdd3e8db12a6e29e
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-21 11:46:32 -0400

    alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,9 +3,17 @@
  * main -  prints the alphabet in lowercase, followed by a new line
  * Return: Always 0 (Success)
  */
-void print_alhabet(void);
+void print_alhabet(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+}
 int main(void)
 {
-    print_alhabet();
-    return (0);
+print_alhabet();
+return (0);
 }
