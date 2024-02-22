39ad119c5be03c5069e09a087f2cda62141d79ea
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-02-22 16:59:49 -0400

    change

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * print_numbers - prints the numbers, from 0 to 9
- *
  * Return: void
  */
 void print_numbers(void)
@@ -13,4 +12,3 @@ _putchar(i + '0');
 }
 _putchar('\n');
 }
-
