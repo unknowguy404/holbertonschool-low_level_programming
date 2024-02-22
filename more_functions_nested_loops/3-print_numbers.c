a8b871617800a1bf40632e7219a780205042ed49
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-02-22 16:52:07 -0400

    change

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,6 @@
 /**
 *print_numbers - function that print all numbers from
 *0 to 9
-*
 *Return: returns nothing
 */
 void print_numbers(void)
@@ -12,5 +11,5 @@ for (number = 48; number < 58; number++)
 {
 _putchar(number);
 }
-_putchar(10);
+_putchar('\n');
 }
