20f175880ce334a05c79f364dc57f827fa36c57c
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-02-22 16:49:03 -0400

    change

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * print_numbers - prints the numbers, from 0 to 9
- *
- * Return: void
- */
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return: returns nothing
+*/
 void print_numbers(void)
 {
-
-int i;
-for (i = 0; i < 10; i++)
+int number = 48;
+for (number = 48; number < 58; number++)
 {
-_putchar(i + '0');
+_putchar(number);
 }
-_putchar('\n');
+_putchar(10);
 }
