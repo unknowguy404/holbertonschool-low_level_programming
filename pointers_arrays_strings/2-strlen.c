a80c738257a4fc4b801bacda14bc91167abd5468
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-26 13:49:17 -0400

    strlen

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
- * _strlen - returns the length of a string.
- * @s: string
- * Return: lenght
+ *_strlen - print a string
+ *@s:is the string to print
+ *Return: lenght of string
 */
 int _strlen(char *s)
 {
 int i = 0;
-while (s[i] != '\0')
-{
+while (s[i] != 'NULL')
 i++;
-}
+return (i);
 }
