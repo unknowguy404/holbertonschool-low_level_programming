dd457f677278b47a30609b1686aa8da18b6a787c
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-26 11:24:57 -0400

    swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * swap_int - function that swaps the values of two integers.
+ * @a: pointer to an int
+ * @b: pointer to an int
+ * return void
+*/
+void swap_int(int *a, int *b)
+{
+*a = b;
+}
