16d79858be433c41f5130f86f32088a445b0ff2f
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-26 16:37:41 -0400

    print in rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse,
+ * followed by a new line
+ * @s: string to print
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int len = 0;
+while (s[len])
+len++;
+for (len--; len >= 0; len--)
+return;
+}
