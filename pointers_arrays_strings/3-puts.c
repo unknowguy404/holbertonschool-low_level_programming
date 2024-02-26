5f11a5f766f1a1ac01f09f139478e0898362a1c7
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-26 15:33:29 -0400

    function that prints

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,14 +1,14 @@
 #include "main.h"
-
 /**
- * _puts - prints a string, followed by a new line, to stdout
- * @str: string to print
- *
- * Return: void
+ * _puts - prints a string followed by a new line
+ * @str: the string to be printed
  */
 void _puts(char *str)
 {
-    while (*str)
-        _putchar(*str++);
-    _putchar('\n');
+    while (*str != '\0')
+    {
+        putchar(*str);
+        str++;
+    }
+    putchar('\n');
 }
