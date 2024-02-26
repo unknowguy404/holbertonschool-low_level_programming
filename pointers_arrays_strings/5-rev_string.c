54b817bbf93de85e112e9a986911a3d2233fcb47
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-02-26 19:54:28 -0400

    rev a string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -2,10 +2,8 @@
 /**
  * _islower - checks for lowercase character
  * @c: character to be checked
- *
  * Return: 1 if c is lowercase, 0 otherwise
  */
-
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
