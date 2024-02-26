3a7c68ae786da64d9ef78a613cfb3c6c2bc20c17
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-26 15:14:05 -0400

    function that prints a string

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _puts - prints a string
- * @str: the string to print
- * Return: void
+ *_puts - prints a string
+ *@str: the string to print
+ *Return: void
  */
 void _puts(char *str)
 {
