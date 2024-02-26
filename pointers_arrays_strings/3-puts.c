82804e3f7291eaca0d44570ea07edda2387a9fe0
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-26 16:09:28 -0400

    function that printsgp

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -4,9 +4,9 @@
  * _puts - prints a string followed by a new line to stdout
  * @str: input string
  */
-void _puts(char str)
+void _puts(char *str)
 {
-    while(str)
+    while(*str)
 
         _putchar(*str++);
     _putchar('\n');
