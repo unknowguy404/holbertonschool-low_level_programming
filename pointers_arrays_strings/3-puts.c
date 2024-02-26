789d5339584fcde419ea90134a94113425832e98
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-26 14:21:44 -0400

    function that prints a string

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,11 +1,14 @@
 #include "main.h"
+
 /**
  * _puts - prints a string
- * @str: the string
-*/
+ * @str: the string to print
+ */
 void _puts(char *str)
 {
 while (*str != '\0')
+{
 _putchar(*str);
 str++;
+}
 }
