e0f23cc598f460f05ba7f4336aed3887b88a75e7
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-02-26 20:02:08 -0400

    puts half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * puts_half - prints half of a string
  * followed by a new line
@@ -10,12 +9,10 @@ void puts_half(char *str)
 int len, n, i;
 
 len = 0;
-
 while (str[len] != '\0')
 {
 len++;
 }
-
 if (len % 2 == 0)
 {
 for (i = len / 2; str[i] != '\0'; i++)
