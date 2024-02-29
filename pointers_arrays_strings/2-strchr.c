ac06d1a2c8c9f79850c2847a1206047707ee4c4e
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-29 13:45:22 -0400

    function that locates a character in a string

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  **_strchr - function that locates a character in a string
  *@s:the string
@@ -11,10 +12,9 @@ int i;
 for (i = 0; s[i] != '\0'; i++)
 {
 if (s[i] == c)
-}
 {
 return (s + i);
-i++;
+}
 }
 return (s);
 }
