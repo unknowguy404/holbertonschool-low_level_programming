3f0fbfd8bbdc2e105229a88bbbce476e6efcbffe
KILLA <killa@MBP-de-KILLA.localdomain>
2024-02-29 14:56:55 -0400

    gets the length of a prefix substring

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_strspn - gets the length of a prefix substring
+ *@s: string to be scanned
+ *@accept: string containing the characters to match
+ *Return: number of bytes in the initial segment of s which consist only of bytes from accept
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+int i, j, flag;
+
+i = 0;
+while (s[i] != '\0')
+}
