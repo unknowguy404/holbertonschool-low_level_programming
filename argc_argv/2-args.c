cb54577d6588b3ba5c0bc67b0506c9e8324d7c69
KILLA <killa@MBP-de-KILLA.localdomain>
2024-03-05 14:14:33 -0400

    update

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -4,13 +4,13 @@
  * main - main function
  * @argc: number of arguments
  * @argv: number of arguments
- * Return: number of arguments* main - main function
+ * Return: number of arguments
  */
 int main(int argc, char *argv[])
 {
 printf("%s\n", argv[0]);
 
-for(int i = 1; i < argc; i++)
+for (int i = 1; i < argc; i++)
 {
 printf("%s\n", argv[i]);
 }
