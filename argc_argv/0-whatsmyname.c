81e356d87af9ab45e4a0905db1233e41ac339983
KILLA <killa@mbp-de-killa.lan>
2024-03-05 11:19:20 -0400

    program that prints its name, followed by a new line

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argcv: number of arguments
+ * Returns: number of arguments
+*/
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
