24616cde211a2b8ffb78cc7efef769a28df27dcd
KILLA <killa@MBP-de-KILLA.localdomain>
2024-03-05 16:46:56 -0400

    update for loop

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+*/
+int main(int argc, char *argv[])
+{
+int value = atoi(argv[1]);
+int value2 = atoi(argv[2]);
+int total = value * value2;
+printf("%d\n",total);
+return (0);
+}
