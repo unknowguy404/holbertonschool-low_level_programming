253c3c68c1d03a41ae7bfac3d9d02e44cbb60ca4
KILLA <killa@MBP-de-KILLA.localdomain>
2024-03-11 12:16:59 -0400

    update a header file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,17 @@
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - dog description
+ * @name:name of the dog
+ * @age:age of the dog
+ * @owner: owner of the dog
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
 };
-
 typedef struct dog dog_t;
+
+#endif
