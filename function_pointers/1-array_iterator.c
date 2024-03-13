73ed06ac790f271b8a06595128575c5ac3c4b778
KILLA <killa@MBP-de-KILLA.localdomain>
2024-03-13 11:58:08 -0400

    function that executes a function given as a parameter on each element of an array

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - iterator for array elements
+ * @array: array
+ * @size: size of array
+ * @action: pointer to function
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+for (size_t i = 0; i < size; i++)
+action(array[i]);
+return;
+}
