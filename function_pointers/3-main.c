8e718e2acd27b7fb69e227a821a43cc231cc396d
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-03-13 14:55:45 -0400

    compiler

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,28 +1,39 @@
 #include "3-calc.h"
+
 /**
- * main - recieves basic calculations arguments
+ * main - receives basic calculations arguments
  * @argc: argument count
  * @argv: argument value
  * Return: Always 0 on success.
  */
 int main(int argc, char *argv[])
 {
-int (*f)(int, int);
+	int (*f)(int, int);
+	int num1, num2;
 
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
-}
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-f = get_op_func(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-if (!f)
-{
-printf("Error\n");
-exit(99);
-}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
-return (0);
+	f = get_op_func(argv[2]);
+
+	if (!f)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", f(num1, num2));
+	return (0);
 }
