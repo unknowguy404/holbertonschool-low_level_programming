73a2eb99d6e2661a4c7d35a91e99c308b9de9b09
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-03-14 19:00:48 -0400

    1

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,11 +2,12 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - function with two arguments
- * @n: int type first argume t
+ * sum_them_all - Returns the sum of the all its parameters.
+ * @n: number of elements of parameters passed to the function
+ * @...: A variable number of parameters to calculate the sum 0
  *
- * Description: returns the sum of all its parameters
- * Return: value of sum or 0
+ * Return: if n == 0 - 0
+ * Otherwise - the sum of all parameters
  */
 int sum_them_all(const unsigned int n, ...)
 {
