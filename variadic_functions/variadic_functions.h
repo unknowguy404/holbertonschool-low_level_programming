e4520a4567415c8055e2fe77c39e672db7a936ca
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-03-14 19:16:48 -0400

    1

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,11 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
 #include <stdarg.h>
-
+/**
+ * struct type - struct for printing data
+ * @type: The data type to be printed
+ * @f: The function pointer to the corresponding function
+ */
 typedef struct types
 {
 	char z;
@@ -10,7 +14,7 @@ typedef struct types
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+void print_all(const char *const format, ...);
 int _putchar(char c);
 
 #endif
