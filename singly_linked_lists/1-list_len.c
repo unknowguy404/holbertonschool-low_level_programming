6593e58cd973cd28164aceb2bf4c1637bb5f21c5
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-03-25 17:20:52 -0400

    adding list len

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,6 +1,8 @@
 #include "lists.h"
 /**
- *
+ * list_len - the length of the list
+ * @h: pointer to the head of the list
+ * Return: the length of the list
  */
 size_t list_len(const list_t *h)
 {
