e749fdf145ba631cccb45ba4170e413be4d11a00
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-03-27 18:59:41 -0400

    fixing code

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,19 +1,24 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * get_bit - get the value of a bit at a given index
- * @n: number to evaluate
- * @index: index starting from 0, of the bit we want to get
- * Return: Value of bit at index, or -1 if error
+ * clear_bit - set the value of a bit to 0 at a given index
+ * @n: pointer to decimal number to change
+ * @index: index position to change
+ * Return: 1 if it worked, -1 if error
  */
-int get_bit(unsigned long int n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int hold;
+	unsigned long int i;
+	unsigned int hold;
 
 	if (index > 64)
 		return (-1);
+	hold = index;
+	for (i = 1; hold > 0; i *= 2, hold--)
+		;
 
-	hold = n >> index;
+	if ((*n >> index) & 1)
+		*n -= i;
 
-	return (hold & 1);
+	return (1);
 }
