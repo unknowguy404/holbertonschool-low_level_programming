d88394036361ab033ca59da55728d197b254933b
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-03-28 12:56:13 -0400

    function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- * append_text_to_file - Appends text at the end of a file.
- * @filename: A pointer to the name of the file.
- * @text_content: The string to add to the end of the file.
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: If the function fails or filename is NULL - -1.
- *         If the file does not exist the user lacks write permissions - -1.
+ * Return: If the function fails - -1.
  *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
@@ -21,12 +20,13 @@ int create_file(const char *filename, char *text_content)
 
 			len++;
 	}
-	o = open(filename, O_WRONLY | O_APPEND);
+	o = open(filename, O_WRONLY | 0_APPEND);
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
-
 		return (-1);
+
 	close(o);
+
 	return (1);
 }
