efaff870551ee4ba49ae61e693a5bd9424244cdc
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-03-28 11:18:43 -0400

    fixing code

--- file_io/main.h
+++ file_io/main.h
@@ -5,11 +5,11 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
-
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
