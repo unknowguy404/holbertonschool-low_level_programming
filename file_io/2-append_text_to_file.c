6f07dfd52054efa6bdca05c3ec51c6dcdba6d475
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-03-28 13:01:56 -0400

    function that appends text at the end of a file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -21,8 +21,8 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	o = open(filename, O_WRONLY | O_APPEND)
-		w = write(o, text_content, len);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
 		return (-1);
