9d888144810b38933995d3daa76c3496ae5f66e3
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-03-28 12:57:04 -0400

    function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,10 +17,10 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
-
 			len++;
 	}
-	o = open(filename, O_WRONLY | 0_APPEND);
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
