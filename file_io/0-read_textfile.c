e078e420a347438859a3e84728b861aea275d2a5
KILLA <killa@MacBook-Pro-de-KILLA.local>
2024-03-28 11:13:02 -0400

    function that reads a text file and prints

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,7 +9,7 @@
  * Return: If the function fails or filename is NULL - 0.
  *         O/w - the actual number of bytes the function can read and print.
  */
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
 	char *buffer;
@@ -23,7 +23,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
-	W = write(STDOUT_FILENO, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, letters);
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
